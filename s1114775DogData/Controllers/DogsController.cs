aaf796ba96ea8ef75327f8dddd8f0f01e706a1f7
max792489 <s1114777@o365st.pu.edu.tw>
2024-10-08 00:46:22 +0800

    加入專案檔案。

---
+++ s1114775DogData/Controllers/DogsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using s1114775DogData.Models;
+
+namespace s1114775DogData.Controllers
+{
+    public class DogsController : Controller
+    {
+        private DogDataEntities db = new DogDataEntities();
+
+        // GET: Dogs
+        public ActionResult Index()
+        {
+            return View(db.Dog.ToList());
+        }
+
+        // GET: Dogs/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Dog dog = db.Dog.Find(id);
+            if (dog == null)
+            {
+                return HttpNotFound();
+            }
+            return View(dog);
+        }
+
+        // GET: Dogs/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Dogs/Create
+        // 若要避免過量張貼攻擊，請啟用您要繫結的特定屬性。
+        // 如需詳細資料，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,variety,color,fur")] Dog dog)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Dog.Add(dog);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(dog);
+        }
+
+        // GET: Dogs/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Dog dog = db.Dog.Find(id);
+            if (dog == null)
+            {
+                return HttpNotFound();
+            }
+            return View(dog);
+        }
+
+        // POST: Dogs/Edit/5
+        // 若要避免過量張貼攻擊，請啟用您要繫結的特定屬性。
+        // 如需詳細資料，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,variety,color,fur")] Dog dog)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(dog).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(dog);
+        }
+
+        // GET: Dogs/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Dog dog = db.Dog.Find(id);
+            if (dog == null)
+            {
+                return HttpNotFound();
+            }
+            return View(dog);
+        }
+
+        // POST: Dogs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Dog dog = db.Dog.Find(id);
+            db.Dog.Remove(dog);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ s1114775DogData/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="s1114775DogData.MvcApplication" Language="C#" %>
